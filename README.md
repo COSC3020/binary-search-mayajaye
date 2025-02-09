16060c0761d2bc06c2c162d512beb64e232b5944
Maya Conway <mconway4@uwyo.edu>
2025-02-09 16:13:48 -0700

    finished binarySearch

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ not there.
 
 Use the template in `code.js`. The tests in `code.test.js` will be run
 automatically every time you push to Github; if they pass you are done.
+
+Help from: Microsoft Copilot
+
+MC: My original binary search did not work with duplicates, so I copied my
+    error and code.test.js to Copilot, and it said that the tests check for 
+    the first occurrence of the element. I added the 'ele' variable to store 
+    the index, but still search the left for an earlier occurrence.
+
+"I certify that I have listed all sources used to complete this exercise, including the use of any Large Language Models. All of the work is my own, except where stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is suspected, charges may be filed against me without prior notice."
+
+
--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-function binarySearch(list, element) {
-    return -1;
+//Maya Conway
+//code.js
+//Binary Search
+//2-9-25
+
+function binarySearch(list,element) {
+    var start = 0;
+    var end = list.length -1;
+    var ele = 0;
+
+    while (start <= end) {              //while the list has more than one element
+        const mid = Math.floor(end / 2);//store the approximate middle of the array
+        if (list[mid] == element) {     //if the middle element is == to element
+            ele = mid;                  //store the index, but check for the first occurrence
+            var found = true;
+            end = mid-1;                //narrow down the list to the left & search again
+        }
+        else if (list[mid] < element) { //if the middle element is < element
+            start = mid + 1;            //narrow down the list to the right &search again
+        }
+        else {
+            end = mid - 1;              //otherwise mid is < and not equal to element
+        }                               //narrow down the list to the left & search again
+    }
+
+    if (!found) return -1;              //not found, return -1
+	else return ele;                    //found is true, return index of element
 }
